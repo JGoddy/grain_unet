11bede2fb12c521e3ed5c6b4f2b8e0fa1f6ec26f
Julian Goddy <jg5091@columbia.edu>
2025-09-23 14:31:22 -0400

    I ran the model for 80 epochs and got graphs of the training and validation loss as a function of epoch

--- utility/plotting.py
+++ utility/plotting.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-def plot_loss_points(loss_points_training, loss_point_validation, interimloss_point, filename_training='lossPlotTraining.png',filename_validation='lossPlotValidation'):
+def plot_loss_points(loss_points_training, loss_point_validation, interimloss_point=None, filename_training='lossPlotTraining.png',filename_validation='lossPlotValidation'):
     '''
 Function that takes in an array of loss function values and exports a png at the end of the session
 '''
-
+ # commenting out interimloss_point for now
     # Create a new figure
     plt.figure()
     
@@ -39,17 +39,17 @@ Function that takes in an array of loss function values and exports a png at the
     #storing data for external plotting
     filename_training_csv = "training_loss_graph_data.csv"
     filename_validation_csv = "validtation_loss_graph_data.csv"
-    filename_interimlosses_csv = "interimlosses_for_Rickman.csv"
+    #filename_interimlosses_csv = "interimlosses_for_Rickman.csv"
 
     df_training_loss_point = pd.DataFrame(loss_points_training, columns=['Epochs'])
 
     df_validation_loss_point = pd.DataFrame(loss_point_validation, columns=['Epochs'])
 
-    df_interimloss_for_Rickman = pd.DataFrame(interimloss_point, columns=['Epochs', 'Interim Loss'])
+    #df_interimloss_for_Rickman = pd.DataFrame(interimloss_point, columns=['Epochs', 'Interim Loss'])
 
     df_training_loss_point.to_csv(filename_training_csv, index=False)
     
     df_validation_loss_point.to_csv(filename_validation_csv,index=False)
 
-    df_interimloss_for_Rickman.to_csv(filename_interimlosses_csv, index=False)
+    #df_interimloss_for_Rickman.to_csv(filename_interimlosses_csv, index=False)
 
